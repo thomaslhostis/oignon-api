97d23a333c74f6f265a60bb770f7a3233e7a69de
Thomas L'Hostis <thomas.lhostis@gmail.com>
2023-12-12 08:47:36 +0100

    API fonctionnelle

--- core/presentation/src/main/kotlin/com/github/thomaslhostis/oignonapi/core/presentation/HomeController.kt
+++ core/presentation/src/main/kotlin/com/github/thomaslhostis/oignonapi/core/presentation/HomeController.kt
@@ -1,4 +1,4 @@
-package com.github.thomaslhostis.oignon.core.presentation
+package com.github.thomaslhostis.oignonapi.core.presentation
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
