4386d40d5ea792757ec5f7c007a4e67d7795129a
Thomas L'Hostis <thomas.lhostis@gmail.com>
2023-12-16 19:35:10 +0100

    Mise en place des tests fonctionnels d'intégration de MongoDB

--- functional-tests/features/src/test/kotlin/com/oignonapi/functionaltests/configuration/ContextCleaner.kt
+++ functional-tests/features/src/test/kotlin/com/oignonapi/functionaltests/configuration/ContextCleaner.kt
@@ -1,6 +1,7 @@
 package com.oignonapi.functionaltests.configuration
 
 import com.oignonapi.functionaltests.features.trainstations.TrainStationsRecord
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
@@ -12,10 +13,16 @@ import org.springframework.web.client.RestTemplate
 @Component
 class ContextCleaner(
     private val testRestTemplate: RestTemplate,
+    private val mongoTemplate: MongoTemplate,
     private val trainStationsRecord: TrainStationsRecord,
 ) {
-    fun cleanContext() {
+    fun cleanup() {
         testRestTemplate.interceptors = emptyList()
+
+        for (collectionName in mongoTemplate.collectionNames) {
+            mongoTemplate.getCollection(collectionName).drop()
+        }
+
         trainStationsRecord.reset()
     }
 }
