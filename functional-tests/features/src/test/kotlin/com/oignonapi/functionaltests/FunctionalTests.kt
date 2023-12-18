c7dd44b30330fe2a5d3fae5919a39327c888e0de
Thomas L'Hostis <thomas.lhostis@gmail.com>
2023-12-18 12:22:38 +0100

    Correction de l'exécution des tests fonctionnels d'intégration MongoDB

--- functional-tests/features/src/test/kotlin/com/oignonapi/functionaltests/FunctionalTests.kt
+++ functional-tests/features/src/test/kotlin/com/oignonapi/functionaltests/FunctionalTests.kt
@@ -2,11 +2,12 @@ package com.oignonapi.functionaltests
 
 import com.oignonapi.OignonApi
 import com.oignonapi.functionaltests.glue.configuration.ContextCleaner
-import io.cucumber.core.options.Constants
+import io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME
 import io.cucumber.java.Before
 import io.cucumber.spring.CucumberContextConfiguration
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
@@ -14,12 +15,8 @@ import org.springframework.test.context.ActiveProfiles
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.oignonapi.functionaltests")
-@ConfigurationParameter(
-    key = Constants.FEATURES_PROPERTY_NAME,
-    value = "../features/src/test/resources/features/trainstations"
-)
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json")
 class FunctionalTests {
     @SpringBootTest(classes = [OignonApi::class], webEnvironment = RANDOM_PORT)
     @CucumberContextConfiguration
--- functional-tests/features/src/test/resources/features/trainstations/Gérer le référentiel de stations de trains.feature
+++ functional-tests/features/src/test/resources/features/trainstations/Gérer le référentiel de stations de trains.feature
@@ -1,6 +1,14 @@
 # language: fr
 Fonctionnalité: Gérer le référentiel de stations de trains
 
+  Scénario: Tenter d'enregistrer deux stations de trains avec le même identifiant
+    Étant données les stations de trains à créer :
+      | Identifiant | Nom        |
+      | STATION_A   | East Blue  |
+      | STATION_A   | North Blue |
+    Lorsque je tente d'enregistrer ces stations de trains
+    Alors je reçois une erreur 400 avec le message "L'identifiant des stations de trains doit être unique"
+
   Scénario: Créer et récupérer les stations de trains
     Étant données les stations de trains à créer :
       | Identifiant | Nom        |
@@ -10,11 +18,3 @@ Fonctionnalité: Gérer le référentiel de stations de trains
     Lorsque j'enregistre ces stations de trains
     Et que je récupère le référentiel des stations de trains
     Alors je reçois ces stations de trains
-
-  Scénario: Tenter d'enregistrer deux stations de trains avec le même identifiant
-    Étant données les stations de trains à créer :
-      | Identifiant | Nom        |
-      | STATION_A   | East Blue  |
-      | STATION_A   | North Blue |
-    Lorsque je tente d'enregistrer ces stations de trains
-    Alors je reçois une erreur 400 avec le message "L'identifiant des stations de trains doit être unique"
