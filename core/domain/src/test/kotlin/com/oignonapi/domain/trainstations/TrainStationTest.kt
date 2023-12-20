d06555592bfd61f85353aa4f2d727bbe550b18f5
Thomas L'Hostis <thomas.lhostis@gmail.com>
2023-12-20 20:35:53 +0100

    Correction d'une coquille

--- core/domain/src/test/kotlin/com/oignonapi/domain/trainstations/TrainStationTest.kt
+++ core/domain/src/test/kotlin/com/oignonapi/domain/trainstations/TrainStationTest.kt
@@ -3,10 +3,12 @@ package com.oignonapi.domain.trainstations
 import com.oignonapi.domain.trainstations.TrainStationNominals.trainStationNominal
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.time.ZonedDateTime
 
 class TrainStationTest {
     @Test
     fun hasOnlyUniqueIdsTest() {
+        println("ZonedDateTime.now()=${ZonedDateTime.now()}")
         val trainStationA = trainStationNominal.copy(id = "A")
         val trainStationB = trainStationNominal.copy(id = "B")
 
